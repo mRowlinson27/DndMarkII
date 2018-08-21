6bd5ef8c7c227140adda272af49cc41a291d96a8
MRowlinson <michaelrowlinson27@gmail.com>
2018-08-21 12:58:57 +1000

    split main window vertically, restyling

--- src/UIModel.API/Dto/Skill.cs
+++ src/UIModel.API/Dto/Skill.cs
@@ -3,8 +3,22 @@ namespace UIModel.API.Dto
 {
     public class Skill
     {
-        public string Type { get; set; }
-        public string Name { get; set; }
         public int Total { get; set; }
+
+        public string Name { get; set; }
+
+        public int Ranks { get; set; }
+
+        public bool HasArmourCheckPenalty { get; set; }
+
+        public int ArmourCheckPenalty { get; set; }
+
+        public bool UseUntrained { get; set; }
+
+        public bool Trained { get; set; }
+
+        public AbilityModifier Modifier { get; set; }
     }
+
+    public enum AbilityModifier { Str, Dex, Con, Int, Wis, Cha };
 }
--- src/UIModel/SkillTableModel.cs
+++ src/UIModel/SkillTableModel.cs
@@ -18,19 +18,310 @@ namespace UIModel
         {
             _skills = new ObservableCollection<Skill>
             {
-                new Skill
+                new Skill()
                 {
-                    Name = "Acro",
-                    Type = "Dex",
-                    Total = Generator.Next(0, 20)
+                    Name = "Acrobatics",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = true,
+                    ArmourCheckPenalty = 0,
+                    Modifier = AbilityModifier.Dex,
+                    UseUntrained = true,
+                    Trained = true
                 },
-                new Skill
+                new Skill()
                 {
-                    Name = "Jump",
-                    Type = "Str",
-                    Total = Generator.Next(0, 20)
+                    Name = "Appraise",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Bluff",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Cha,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Climb",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Str,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Diplomacy",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Cha,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Disable Device",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Dex,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Disguise",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Cha,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Escape Artist",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Dex,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Fly",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Dex,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Handle Animal",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Cha,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Heal",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Wis,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Intimidate",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Cha,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Linguistics",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Perception",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Wis,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Ride",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Dex,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Sense Motive",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Wis,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Sleight of Hand",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Dex,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Spellcraft",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Stealth",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Dex,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Survival",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Wis,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Swim",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Str,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Use Magic Device",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Cha,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (arcana)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (dungeoneering)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (engineering)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (geography)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (history)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (local)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (nature)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (nobility)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (planes)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Knowledge (religion)",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Int,
+                    UseUntrained = true,
+                    Trained = false
+                },
+                new Skill()
+                {
+                    Name = "Profession",
+                    Ranks = 0,
+                    HasArmourCheckPenalty = false,
+                    Modifier = AbilityModifier.Wis,
+                    UseUntrained = true,
+                    Trained = false,
                 }
             };
+
+            foreach (var skill in _skills)
+            {
+                skill.Total = Generator.Next(0, 20);
+            }
         }
 
         public async Task AddSkillAsync()
@@ -51,7 +342,7 @@ namespace UIModel
             return new Skill
             {
                 Name = "New Skill",
-                Type = "Str",
+                Modifier = AbilityModifier.Con,
                 Total = Generator.Next(0, 20)
             };
         }
