166357865b9e7dbb56767cf6a45274f49e31b85a
MRowlinson <michaelrowlinson27@gmail.com>
2018-08-22 16:09:49 +1000

    add new BootStrapper project

---
+++ src/BootStrapper/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿
+namespace BootStrapper
+{
+    using System;
+    using Chromium.Event;
+    using Neutronium.Core.JavascriptFramework;
+    using Neutronium.JavascriptFramework.Knockout;
+    using Neutronium.WebBrowserEngine.ChromiumFx;
+    using Neutronium.WPF;
+    using Utilities.Implementation;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : ChromiumFxWebBrowserApp
+    {
+        public UIView.MainWindow MainWindow { get; set; }
+
+        [STAThread]
+        static void Main()
+        {
+            var logger = LoggerFactory.GetInstance;
+            logger.LogMessage("Program started");
+
+            using (var bootstrapper = new UIView.BootStrapper(logger))
+            {
+                var application = bootstrapper.SetupApplication();
+                var mainWindow = bootstrapper.CreateMainWindow();
+
+                application.MainWindow = mainWindow;
+
+                logger.LogMessage("Bootstrapping complete");
+
+                application.Run(mainWindow);
+            }
+
+            logger.LogMessage("Program closed\n");
+        }
+
+        protected override void OnStartUp(IHTMLEngineFactory factory)
+        {
+            base.OnStartUp(factory);
+            MainWindow.Init();
+        }
+
+        //These do nothing
+        protected override IJavascriptFrameworkManager GetJavascriptUIFrameworkManager()
+        {
+            return new KnockoutFrameworkManager();
+        }
+
+        //These do nothing
+        protected override void UpdateLineCommandArg(CfxOnBeforeCommandLineProcessingEventArgs beforeLineCommand)
+        {
+        }
+    }
+}
