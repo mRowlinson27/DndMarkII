4e160aef68ecaa60a0ae65b09bac016f695bb9f8
MRowlinson <michaelrowlinson27@gmail.com>
2018-08-24 09:12:15 +1000

    add basic UIView.UnitTests

---
+++ tests/UIView.UnitTests/SkillTableViewModelTests.cs
@@ -0,0 +1,96 @@
+﻿
+namespace UIView.UnitTests
+{
+    using System;
+    using System.ComponentModel;
+    using FakeItEasy;
+    using NUnit.Framework;
+    using UIModel.API;
+    using UIModel.API.Dto;
+    using UIUtilities;
+    using UIUtilities.API;
+    using UIUtilities.API.AsyncCommands;
+    using UIUtilities.AsyncCommands;
+    using Utilities.API;
+    using ViewModel;
+
+    [TestFixture]
+    public class SkillTableViewModelTests
+    {
+        private SkillTableViewModel _skillTableViewModel;
+
+        private ILogger _logger;
+        private ISkillTableModel _skillTableModel;
+        private IObservableBinder _observableBinder;
+        private IAsyncCommandFactory _asyncCommandFactory;
+        private IAsyncTaskRunnerFactory _asyncTaskRunnerFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = A.Fake<ILogger>();
+            _skillTableModel = A.Fake<ISkillTableModel>();
+            _observableBinder = new ObservableBinder();
+            var notifyTaskCompletionFactory = A.Fake<INotifyTaskCompletionFactory>();
+            _asyncCommandFactory = new AsyncCommandFactory(notifyTaskCompletionFactory);
+            _asyncTaskRunnerFactory = new AsyncTaskRunnerFactory(notifyTaskCompletionFactory);
+
+            _skillTableViewModel = new SkillTableViewModel(_logger, _skillTableModel, _observableBinder, _asyncCommandFactory, _asyncTaskRunnerFactory);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _skillTableViewModel.Dispose();
+        }
+
+        [Test]
+        public void Init_RequestsSkillData()
+        {
+            //Arrange
+
+            //Act
+            _skillTableViewModel.Init();
+
+            //Assert
+            A.CallTo(() => _skillTableModel.RequestSkillsAsync()).MustHaveHappened();
+        }
+
+        [Test]
+        public void Model_OnPropertyChanged_RequestsSkillData()
+        {
+            //Arrange
+
+            //Act
+            _skillTableModel.PropertyChanged += Raise.FreeForm<PropertyChangedEventHandler>.With(_skillTableModel, new PropertyChangedEventArgs("Skills"));
+
+            //Assert
+            A.CallTo(() => _skillTableModel.RequestSkillsAsync()).MustHaveHappened();
+        }
+
+        [Test]
+        public void AddSkill_RequestsFromModel()
+        {
+            //Arrange
+
+            //Act
+            _skillTableViewModel.AddSkill.Execute(null);
+
+            //Assert
+            A.CallTo(() => _skillTableModel.AddSkillAsync()).MustHaveHappened();
+        }
+
+        [Test]
+        public void RemoveSkill_RequestsFromModel()
+        {
+            //Arrange
+            var skill = new Skill();
+
+            //Act
+            _skillTableViewModel.RemoveSkill.Execute(skill);
+
+            //Assert
+            A.CallTo(() => _skillTableModel.RemoveSkillAsync(skill)).MustHaveHappened();
+        }
+    }
+}
